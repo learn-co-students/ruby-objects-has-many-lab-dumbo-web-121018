52bea0cf9cc06945523737983f8246c3ef88370e
reduviidae <wiljago@gmail.com>
2018-12-14 13:20:20 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require "pry"
+
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all.select do |song|
+    self == song.artist
+    end
+  end
+
+  def add_song(song)
+    # binding.pry
+    song.artist = self
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+    # binding.pry
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
