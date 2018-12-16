36738af997339f0fed36201e0bfe5f7415ece7b9
milespucarelli <miles.pucarelli@gmail.com>
2018-12-16 13:25:40 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  attr_reader :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    song.artist = self
+    self.songs << song
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+
+  def self.song_count
+    self.all.inject(0) {|count, artist| count + artist.songs.count}
+  end
+end
